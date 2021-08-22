a97ffe90409f758d72c4fc4adc80991774af5cd2
rociobelenmartinez <88633829+rociobelenmartinez@users.noreply.github.com>
2021-08-22 19:59:52 -0300

    cambios entrega1

--- js/login.js
+++ js/login.js
@@ -1,6 +1,6 @@
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
-
-});
+function validar(){
+    let nombre = document.getElementById("nombre").value;
+    let contrasena = document.getElementById("contrasena").value;
+    if((nombre !=="")&& (contrasena !== "")){ 
+        window.location.href = "menu.html"}
+}
