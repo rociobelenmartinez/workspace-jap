d53f30f806ea2d2377516f9a3d3affa9cf4ed83a
rociobelenmartinez <88633829+rociobelenmartinez@users.noreply.github.com>
2021-09-17 21:05:54 -0300

    jsonComments

--- js/product-info.js
+++ js/product-info.js
@@ -45,3 +45,56 @@ document.addEventListener("DOMContentLoaded", function(e){
         }
     });
 });
+//Funcion de mostrar ocmentarios
+var jsonComments = [];
+
+function showProductsJsonComments(){
+
+    let htmlContentToAppend = "";
+    for(let i = 0; i < jsonComments.length; i++){
+        let comment = jsonComments[i];
+
+
+
+            htmlContentToAppend += `
+            
+                <div class="row">
+
+                    <div class="col">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h4 class="mb-1">`+ comment.user +`</h4>
+                            
+                        </div>
+                        <p class="mb-1">` + comment.description + `</p>
+                        <div> 
+                    
+                        <p class="mb-1">` + "puntuación:" + " " + comment.score +`</p>
+                        </div>
+                        
+                        <div> 
+                        <p class="mb-1">`+ comment.dateTime +`</p>
+                        </div>
+
+
+                    </div>
+
+                </div>
+            
+            `
+        }
+
+        document.getElementById("productJsonComments").innerHTML = htmlContentToAppend;
+    }
+
+
+
+    document.addEventListener("DOMContentLoaded", function(e){
+        getJSONData(PRODUCT_INFO_COMMENTS_URL).then(function(resultObj){
+            if (resultObj.status === "ok"){
+
+                jsonComments = resultObj.data; 
+                showProductsJsonComments(jsonComments);
+            }
+        });
+        
+    });
